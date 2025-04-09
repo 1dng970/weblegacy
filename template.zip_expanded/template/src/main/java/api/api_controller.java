8ea4811fc82948b28a1887cb5801f4d64a1f60d6
1dng970 <1dng970@naver.com>
2025-04-09 10:09:33 +0900

    fvlkadhglajdgja;s

--- template.zip_expanded/template/src/main/java/api/api_controller.java
+++ template.zip_expanded/template/src/main/java/api/api_controller.java
@@ -1,6 +1,8 @@
 package api;
 
 import java.io.PrintWriter;
+
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -27,397 +29,395 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+//import org.mybatis.spring.SqlSessionFactoryBean;
+/*
+ jackson =>ajax=>JSON
+ 예 )/api/data.do/{data} GSON 라이브러리
+ @PutMapping C
+ @DeleteMapping  D
+ @PatchMapping   U
+ 
+ @GetMapping @PostMapping =>JSON 허락 예) /api/data.do
+ */
+
+
+
 
 //import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
 @Controller
 public class api_controller {
-   // log로 문제사항 및 실행사항을 체크하는 라이브러리
-   private static final Logger logger = LoggerFactory.getLogger(api_controller.class);
-
-   /*
-    this.logger.info : 해당 메소드에서 실행된 값을 출력하는 역할
-    this.logger.error : catch에서 사용하는 형태 error 발생시 출력되는 메세지
-    this.logger.debug : 해당 코드가 정상적으로 작동하는 테스트 메세지를 출력
-    this.logger.trace : 해당 코드에 문제가 발생시 좀 더 상세하게 문제사항을 출력
-    this.logger.warn : 현재 코드에 대해서 향후 문제가 발생될 수 있는 원인에 대한 메세지 출력
-    this.logger.fatal : 치명적인 오류 발생시 출력되는 역할
-    */
-      
-   
-   PrintWriter pw = null;
-   
-   //@RequestBody String data : 정상적으로 값을 받아서 출력확인 
-   @PutMapping("/ajax/ajax14/{key}") //insert(DTO)기본
-   public String ajax14(HttpServletResponse res,
-		   @PathVariable("key")String key,
-		   @ModelAttribute api_dto dto		 
-		 ) {
-	   try {		 	
-			 this.pw = res.getWriter();
-	         if(key.equals("a123456")) {
-	        	 //this.logger.info(data.toString());
-	        	this.logger.info(dto.getPd1().toString());
-	        	 this.pw.write("ok");
-	        	 
-	         }else {
-	        	 this.pw.write("key error");
-	         }
-				        	 
-		} catch (Exception e) {
-					
-		}
-	   
-	   return null;
-   }
-   
-   //@RequestPart MultipartFile 을 사용해야 한다
-   //@RequestParam : name 또는 파라미터 
-   //@ResponseBody + @Mapping : method 선언시 사용 
-   //@ResponseBody : 응답에 대한 결과값을 해당 메소드에 바로 출력 할때 사용 
-   //@RequestBody : 배열값을 처리하는 어노테이션 입니다.
-   @DeleteMapping("/ajax/ajax13/{key}")  //delete
-   public String ajax13(HttpServletResponse res,
-		   @PathVariable(name="key") String key,	
-		   
-		   @RequestParam Map<Object,Object> midx	
-		  
-		   /*
-		   @RequestBody String midx
-		   */
-		   ) {
-	   try {		 
-			 this.pw = res.getWriter();
-	         if(key.equals("a123456")) {
-                 //this.logger.info(midx.size);
-                 System.out.println(midx);
-	        	 this.pw.write("ok");
-	        	 
-	         }else {
-	        	 this.pw.write("key error");
-	         }
-				        	 
-		} catch (Exception e) {
-					
-		}
-	   
-	   return null;
- }
-   
-   /*
-   //@PathVariable :URL 파라미터 값을 가져오는 어노테이션 {id} 가상의 파라미터값 
-   JSON.stringify에 대한 정보값을 처리하지 못함 
-   */
-   
-   @PatchMapping("/ajax/ajax12.do/{data}")
-   public String ajax12(HttpServletResponse res,
-		   @PathVariable(name="data") String data,
-		   @RequestBody String myinfo) {
-	 	 try {	
-	 		 if(data.equals("patch_myinfo")) {
-	 			 this.logger.info(myinfo);
-	 			 
-	 		 }else {
-	 			 this.pw.write("error");	 			 
-	 		 }
-	 		 
-	 		 
-	 		 
-         //this.logger.info(mid);
-	 		//this.logger.info(mid.toString());
-	 		 //String user[] =mid.split(",");
-	 		 //this.logger.info(user[1]);
-	 		 //this.logger.info(user[2]);
-	 		 
-		 this.pw = res.getWriter();
-	     this.pw.write("ok");
-		
-	} catch (Exception e) {
-				
-	}
-	   
-	 return null;  
- }
- 
- 
- 
-
-  
-     
-  
-   //ECMA - POST통신 (배열방식) - array 방식	   
-   @PostMapping("/ajax/ajax11.do")
-   /*
-   //JSON.stringify => 전송시  @RequestBody
-   public String ajax11(HttpServletResponse res,
-		   @RequestBody String data) {
+	
+	//log로 문제사항 및 실행사항을 체크하는 라이브러리
+	private static final Logger logger = LoggerFactory.getLogger(api_controller.class);
+	
+	/*
+	this.logger.info : 해당 메소드에서 실행된 값을 출력하는 역활
+	this.logger.error : catch에서 사용하는 형태 error 발생시 출력되는 메세지
+	this.logger.debug : 해당 코드가 정상적으로 작동하는 테스트 메세지를 출력
+	this.logger.trace : 해당 코드에 문제가 발생시 좀 더 상세하게 문제사항을 출력
+	this.logger.warn : 현재 코드에 대해서 향후 문제가 발생 될 수 있는 원인에 대한 메세지 출력
+	this.logger.fatal : 치명적인 오류 발생시 출력되는 역활
 	*/
-   /*
-   public String ajax11(HttpServletResponse res,
-		   @RequestParam("mid")String mid, @RequestParam("mname") String mname) {	
-		   */  
-   
-   public String ajax11(HttpServletResponse res,
-		   @ModelAttribute api_dto dto) {	
-   
-	 	 try {		 
-	 	 //this.logger.info(dto.getMid().toString());
-	 	 //this.logger.info(dto.getMname().toString());	 
-		 //this.logger.info(dto.getMage().toString());
-		 this.pw = res.getWriter();
-	     this.pw.write("ok");
-		
-	} catch (Exception e) {
+	PrintWriter pw = null;
+	
+	//@RequestBody String data : 정상적으로 값을 받아서 출력확인
+	@PutMapping("/ajax/ajax14/{key}")	//insert (DTO 기본)
+	public String ajax14(HttpServletResponse res,
+			@PathVariable(name = "key")String key,
+			//@ModelAttribute api_dto dto
+			@RequestBody String data
+			) {
+		try {
+			this.pw = res.getWriter();
+			if(key.equals("a123456")) {
+				this.logger.info(data);
+				Map<String, String> mp = new HashMap<String, String>();
+				JSONObject jo = new JSONObject(data);
+				Iterator<String> keys=jo.keys();
+				while(keys.hasNext()) {
+					String keynm = keys.next();//키명
+					mp.put(keynm, jo.getString(keynm).toString());					
+				}
+				this.logger.info(mp.toString());
 				
-	}
-	   
-	 return null;  
-   }
-   
-   
-   
-   
-   
-   
- //ECMA - Ajax(POST)
-   @PostMapping("/ajax/ajax10.do")
-   public String ajax10(HttpServletResponse res,
-		   @RequestParam(name="mid")String mid
-		   ) {
-	 try {		   
-		 this.pw = res.getWriter();
-	     this.pw.write("ok");
-		
-	} catch (Exception e) {
 				
+				//this.logger.info(dto.toString());
+				this.pw.write("ok");
+			}else {
+				this.pw.write("key error");
+			}
+			
+		}catch (Exception e) {
+			
+		}
+		return null;
 	}
-	   
-	 return null;  
-   }
-   
-     
-   //ECMA - Ajax(GET)
-   @GetMapping("/ajax/ajax9.do")
-   public String ajax9(HttpServletResponse res,
-		   @RequestParam(name="mid") String mid
-		   ) {
-	 try {		   
-		 this.pw = res.getWriter();
-	     this.pw.write("ok");
-		
-	} catch (Exception e) {
+	//@RequestPart : MultipartFile
+	//@RequestParam : name 또는 파라미터
+	/*
+	@ResponseBody + @Mapping : method 선언시에 사용
+	@ResponseBody : 응답에 대한 결과값을 해당 메소드에 바로 출력할 때 사용
+	*/
+	//@RequestBody : 배열값을 처리하는 어노테이션 입니다.
+	@DeleteMapping("/ajax/ajax13/{key}")	//delete
+	public String ajax13(HttpServletResponse res,
+			@PathVariable(name = "key")String key,
+			@RequestBody String midx){
+		try {
+			this.pw = res.getWriter();
+			if(key.equals("a123456")) {
+				System.out.println(midx);
 				
+				this.pw.write("ok");
+			}else {
+				this.pw.write("key error");
+			}
+			
+		}catch (Exception e) {
+			
+		}
+		return null;
 	}
-	   
-	 return null;  
-   }
-   
+	
 
-   
-   @PostMapping("/ajax/ajax8.do")
-   public String ajax8(
-         @RequestParam(value = "fdata", defaultValue = "", required = false) String fdata,
-         HttpServletResponse res
-         ) throws Exception{
-      try {
-    	 System.out.println(fdata);
-         String rdata[] = fdata.split(",");
-         this.logger.info(rdata[0]);         
-         this.pw = res.getWriter();
-         this.pw.write("ok");
-      }
-      catch(Exception e) {
-         
-      }
-      return null;
-   }
-   
-   // 각각의 다른 키로 POST 전송시 (Jquery)
-   // Front-end에서 파라미터 형태로 문자열 기준으로 POST 전송시 Back-end에서는 dto 활성화
-   @PostMapping("/ajax/ajax7.do")
-   public String ajax7(api_dto dto, ServletResponse res) {
-      try {
-         //this.logger.info(dto.getPd1());
-         //this.logger.info(dto.getPd4());
-         
-         this.pw = res.getWriter();
-         this.pw.write("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      
-      return null;
-   }
-
-   // 각각의 다른 키로 데이터를 받아서 처리 (JSON.stringify)
-   /*
-   @PostMapping("/ajax/ajax7.do")
-   public String ajax7(@RequestBody String alldata, ServletResponse res) {
-      try {
-         this.logger.info(alldata);
-         
-         JSONObject jo = new JSONObject(alldata);
-         
-         this.logger.info(jo.keys().toString());
-         this.logger.info(jo.getString("pd1"));
-         
-         Iterator i =  jo.keys();   // key 이름을 순차적으로 가져오는 인터페이스
-         while(i.hasNext()) {
-            String a  = i.next().toString();   // 키 이름값
-            this.logger.info(a);
-            this.logger.info(jo.getString(a));   // 해당 키 값의 데이터
-         }
-         
-         this.pw = res.getWriter();
-         this.pw.write("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      
-      return null;
-   }
-   */
-   
-   // JQuery-Ajax (POST) => JSON.stringify
-   @PostMapping("/ajax/ajax6.do")
-   public String ajax6(@RequestBody String all_data, ServletResponse res) {
-      try {
-         this.logger.info(all_data);
-         
-         // 대표키가 있는 경우
-         JSONObject jo = new JSONObject(all_data);
-         this.logger.info(jo.get("userdata").toString());
-         
-         /*
-         // 대표키가 없을 경우
-         JSONArray ja = new JSONArray(all_data);
-         this.logger.info(ja.getString(0).toString());
-         */
-         
-         this.pw = res.getWriter();
-         this.pw.write("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      
-      return null;
-   }
-   
-   // Jquery - 배열 값을 GET으로 받아서 처리한 메소드
-   @GetMapping("/ajax/ajax5.do")
-   public String ajax5(@RequestParam("no")String no, ServletResponse res) {
-      try {
-         this.logger.info(no);
-         
-         JSONArray ja = new JSONArray(no);   // []
-         
-         int w = 0;
-         while(w < ja.length()) {
-            this.logger.info(ja.get(w).toString());
-            w++;
-         }
-         
-         this.pw = res.getWriter();
-         this.pw.write("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      
-      return null;
-   }
-   
-   // JSON.stringify : Front-end가 전송시 무조건 @RequestBody로 처리
-   @PostMapping("/ajax/ajax4.do")
-   public String ajax4(ServletResponse res, @RequestBody String pd) {   
-      
-      try {
-         // this.logger.info(dto.getPd1());
-         // this.logger.info(dto.getPd2());
-         
-         this.logger.info(pd);
-         
-         JSONArray ja = new JSONArray(pd);
-         
-         int w = 0;
-         this.logger.info(String.valueOf(ja.length()));
-         while(w < ja.length()) {
-            JSONObject jo = (JSONObject)ja.get(w);
-            String usernm = jo.get("pd"+(w + 1)).toString();
-            this.logger.info(usernm);
-            w++;
-         }
-         
-         this.pw = res.getWriter();
-         this.pw.print("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      return null;
-   }
-   
-   /*
-    @RequestHeader : Ajax에서만 사용하는 Headers의 값이며, 키에 맞는 데이터를 가져올 수 있음
-    Front-end : setRequestHeader에 key, value 값을 보낼 경우에만 사용함
-    
-    @RequestBody : content-type,application/json 전송이 되었을 경우
-    */
-   
-   @PostMapping("/ajax/ajax3.do")
-   public String ajax3(ServletResponse res,
-         // @RequestHeader(name="User") String user,
-         @RequestBody String pd) {   
-      // this.logger.info(user);
-      this.logger.info(pd);
-      
-      try {
-         this.pw = res.getWriter();
-         this.pw.print("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      return null;
-   }
-   
-   // Array 형태로 전송시 GET 형태의 메소드와 동일하게 POST 값을 처리하면 됨
-   @PostMapping("/ajax/ajax2.do")
-   public String ajax2(ServletResponse res,
-         @RequestParam(name="person") String person,
-         @RequestParam(name="product") String pd[]) {   
-      try {
-         this.logger.info(person);
-         this.logger.info(pd[0]);
-         this.pw = res.getWriter();
-         this.pw.print("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      return null;
-   }
-   
-   // js - Ajax (GET)
-   // 문자열 + ok, no, error => GET (product)
-   @GetMapping("/ajax/ajax1.do")
-   public String ajax1(@RequestParam(name="product") String data[], HttpServletResponse res) {
-      // Front-end에서 보낸 name을 원시배열로 받을 경우 자동으로 배열로 변경처리
-      // 단, String으로 배열 자료형을 사용하지 않을 경우 split을 이용하여 값을 분리 시켜야 함
-      
-      this.logger.info(data[0]);
-      // this.logger.info(data);
-      
-      try {
-         this.pw = res.getWriter();
-         this.pw.print("ok");
-      }
-      catch(Exception e) {
-         this.logger.error(e.toString());
-      }
-      return null;
-   }
+	
+	/*
+	 @PathVariable : URL 파라미터 값을 가져오는 어노테이션 {id} 가상의 파라미터 값
+	 JSON.stringify에 대한 정보값을 처리하지 못함
+	 */
+	@PatchMapping("/ajax/ajax12.do/{data}")
+	public String ajax12(HttpServletResponse res,
+			@PathVariable(name="data") String data,
+			@RequestBody String myinfo) {
+		try {
+			this.pw = res.getWriter();
+			if(data.equals("patch_myinfo")) {
+				this.logger.info(myinfo);				
+				this.pw.write("ok");
+			}
+			else {
+				this.pw.write("error");	
+			}
+		
+			//this.logger.info(mid);
+			//String user[] = mid.split(",");
+						
+		
+		}catch(Exception e) {
+			
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//ECMA - POST통신 (배열방식) - array
+	@PostMapping("/ajax/ajax11.do")
+	
+	//JSON.stringify => 전송시 @RequestBody
+	/*
+	public String ajax11(HttpServletResponse res,
+			@RequestBody String data) {
+	*/
+	/*
+	public String ajax11(HttpServletResponse res,
+			@RequestParam("mid") String mid, @RequestParam("mname") String mname) {
+	*/
+	public String ajax11(HttpServletResponse res,
+			@ModelAttribute api_dto dto) {
+		try {
+			this.logger.info(dto.getMid().toString());
+			this.logger.info(dto.getMname().toString());
+			this.logger.info(dto.getMage().toString());
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch(Exception e) {
+			
+		}
+		return null;
+	}
+		
+	
+	
+	//ECMA - POST통신
+	@PostMapping("/ajax/ajax10.do")
+	public String ajax10(HttpServletResponse res,@RequestParam(name="mid")String mid) {
+		try {
+			this.logger.info(mid);
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch(Exception e) {
+			
+		}
+		return null;
+	}
+	
+	
+	//ECMA - Ajax(GET)
+	@GetMapping("/ajax/ajax9.do")
+	public String ajax9(HttpServletResponse res,
+			@RequestParam(name="mid") String mid
+			) {
+		try {
+			this.logger.info(mid);
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch(Exception e) {
+			
+		}
+		return null;
+	}
+		
+	
+	//value = "name값명", defaultValue="기본값", required=false (필수가 아님) - Ajax FormData()
+	@PostMapping("/ajax/ajax8.do")  
+	public String ajax8(@RequestParam(value="fdata", defaultValue = "", required = false) String fdata, 
+		HttpServletResponse res) throws Exception{
+		try {
+					
+			System.out.println(fdata);
+			String rdata[] = fdata.split(",");
+			this.logger.info(rdata[0]);
+			this.pw = res.getWriter();
+			this.pw.write("ok");	
+		}catch (Exception e) {
+			
+		}
+		return null;
+	}
+	
+	
+	
+	
+	//각각의 다른키로 POST 전송시 (Jquery) - DTO
+	//Front-end에서 파라미터 형태로 문자열 기준으로 POST 전송시 Backend에서는 dto 활성화
+	@PostMapping("/ajax/ajax7.do")
+	public String ajax7(api_dto dto,ServletResponse res) {
+		try {		
+			this.logger.info(dto.getPd1());
+			this.pw = res.getWriter();
+			this.pw.write("ok");	
+		}catch (Exception e) {
+			
+		}
+		return null;
+	}
+	
+	
+	//각가의 다른키로 데이터를 받아서 처리 (JSON.stringify)
+	/*
+	@PostMapping("/ajax/ajax7.do")
+	public String ajax7(@RequestBody String alldata,ServletResponse res) {
+		try {		
+			this.logger.info(alldata);
+			JSONObject jo = new JSONObject(alldata);
+			
+			Iterator i = jo.keys();		//키이름을 순차적으로 가져오는 인터페이스 
+			while(i.hasNext()) {
+				String a = i.next().toString();	//키 이름값
+				this.logger.info(a);
+				this.logger.info(jo.getString(a));	//해당 키값의 데이터		
+			}
+			
+			this.pw = res.getWriter();
+			this.pw.write("ok");	
+		}catch (Exception e) {
+			
+		}
+		return null;
+	}
+	*/
+	
+	
+	//JQuery-Ajax (POST) => JSON.stringify (대표키가 없는 경우
+	@PostMapping("/ajax/ajax6.do")
+	public String ajax6(@RequestBody String all_data,ServletResponse res) {
+		try {
+			this.logger.info(all_data);
+			//대표키가 있는 경우
+			JSONObject jo = new JSONObject(all_data);
+			this.logger.info(jo.get("userdata").toString());
+			
+			/*	//대표키가 없을 경우
+			JSONArray ja = new JSONArray(all_data);
+			this.logger.info(ja.get(0).toString());
+			*/
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch (Exception e) {
+			
+		}
+		
+		return null;
+	}
+	
+	
+	
+	//Jquery - 배열값을 GET으로 받아서 처리한 메소드
+	@GetMapping("/ajax/ajax5.do")
+	public String ajax5(@RequestParam("no")String no, ServletResponse res) {
+		try {
+			this.logger.info(no);
+			JSONArray ja = new JSONArray(no); //[]
+			int w = 0;
+			while(w < ja.length()) {
+				this.logger.info(ja.get(w).toString());
+				w++;
+			}
+			
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+		}catch (Exception e) {
+			
+		}
+		
+		return null;
+	}
+	
+		
+	
+	
+	//JSON.stringify : Front-end가 전송시 무조건 @RequestBody 처리
+	@PostMapping("/ajax/ajax4.do")
+	public String ajax4(ServletResponse res,@RequestBody String pd) {
+		try {
+			this.logger.info(pd);
+			JSONArray ja = new JSONArray(pd);
+			int w = 0;
+			this.logger.info(String.valueOf(ja.length()));
+			while(w < ja.length()) {
+				JSONObject jo = (JSONObject)ja.get(w);
+				String usernm = jo.get("pd"+(w+1)).toString();
+				this.logger.info(usernm);
+				w++;
+			}
+			
+			
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch(Exception e) {
+			this.logger.error(e.toString());
+		}
+		return null;
+	}
+	
+	/*
+	 @RequestHeader : Ajax에서만 사용하는 Headers의 값이며, 키에 맞는 데이터를 가져올 수 있음
+	 Front-end : setRequestHeader에 key, value값을 보낼 경우에만 사용함
+	
+	 
+	 @RequestBody : content-type,application/json 전송이 되었을 겨우
+	 */
+	
+	@PostMapping("/ajax/ajax3.do")
+	public String ajax3(ServletResponse res,
+		@RequestBody String pd		
+		) {
+		//@RequestHeader(name = "User") String user
+		//this.logger.info(user);
+		
+		this.logger.info(pd);
+		try {
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+			
+		}catch(Exception e) {
+			this.logger.error(e.toString());
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	//Array 형태로 전송시 GET 형태의 메소드와 동일하게 POST 값을 처리하면 됨
+	@PostMapping("/ajax/ajax2.do")
+	public String ajax2(ServletResponse res, 
+			@RequestParam(name="person") String person,
+			@RequestParam(name="product") String pd[]) {
+		try {
+			this.logger.info(person);
+			this.logger.info(pd[0]);
+			this.pw = res.getWriter();
+			this.pw.write("ok");
+		}catch(Exception e) {
+			this.logger.error(e.toString());
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	//js - Ajax(GET)
+	//문자열 + ok, no, error => GET (product)
+	@GetMapping("/ajax/ajax1.do")
+	public String ajax1(@RequestParam(name="product") String data[], ServletResponse res) {
+		//Front-end에서 보낸 name을 원시배열로 받을 경우 자동으로 배열로 변경처리
+		//단, String으로 배열 자료형을 사용하지 않을 경우 split을 이용하여 값을 분리 시켜야함
+		this.logger.info(data[0]);
+		try {
+			this.pw = res.getWriter();
+			this.pw.print("ok");
+		}
+		catch(Exception e) {
+			this.logger.error(e.toString());
+		}
+		
+		return null;
+	}
+	
+	
 }
---
+++ template.zip_expanded/template/src/main/java/api/api_mapper.java
@@ -0,0 +1,8 @@
+package api;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface api_mapper {
+
+}
